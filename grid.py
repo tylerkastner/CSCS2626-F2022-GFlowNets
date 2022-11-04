38c165f47da8544739226c640a4761952cdc070d
Binbose <luca.thiede@yahoo.com>
2022-11-04 14:28:11 -0400

    added config

--- grid.py
+++ grid.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import torch
 import tqdm
+import yaml
+import munch
 from gfn.envs import HyperGrid
 from gfn.estimators import LogitPFEstimator, LogitPBEstimator, LogZEstimator
 from gfn.losses import TBParametrization, TrajectoryBalance
@@ -8,16 +10,12 @@ from gfn.samplers import DiscreteActionsSampler, TrajectoriesSampler
 from gfn.containers.replay_buffer import ReplayBuffer
 from gfn.utils import trajectories_to_training_samples, validate
 
-batch_size = 32
-use_replay_buffer = False
-replay_buffer_size = 1000
 
-validation_interval = 100
-resample_for_validation = True
-n_validation_samples = 1000
+with open("config.yml", "r") as ymlfile:
+    config = yaml.safe_load(ymlfile)
+config = munch.munchify(config)
 
-grid_height = 8
-env = HyperGrid(ndim=2, height=grid_height, R0=0.01)  # Grid of size 8x8x8x8
+env = HyperGrid(ndim=config.env.ndim, height=config.env.height, R0=0.01)  # Grid of size 8x8x8x8
 env.render_gt()
 
 logit_PF = LogitPFEstimator(env=env, module_name='NeuralNet')
@@ -34,10 +32,10 @@ loss_fn = TrajectoryBalance(parametrization=parametrization)
 replay_buffer = None
 
 visited_terminating_states = (
-    env.States.from_batch_shape((0,)) if not resample_for_validation else None
+    env.States.from_batch_shape((0,)) if not config.experiment.resample_for_validation else None
 )
-if use_replay_buffer > 0:
-    replay_buffer = ReplayBuffer(env, loss_fn, capacity=replay_buffer_size)
+if config.experiment.use_replay_buffer > 0:
+    replay_buffer = ReplayBuffer(env, loss_fn, capacity=config.experiment.replay_buffer_size)
 
 params = [
     {"params": [val for key, val in parametrization.parameters.items() if key != "logZ"],"lr": 0.001},
@@ -48,11 +46,11 @@ optimizer = torch.optim.Adam(params)
 states_visited = 0
 unique_visited_states = {}
 for i in tqdm.trange(1000):
-    trajectories = trajectories_sampler.sample(n_trajectories=batch_size)
+    trajectories = trajectories_sampler.sample(n_trajectories=config.experiment.batch_size)
     training_samples = trajectories_to_training_samples(trajectories, loss_fn)
     if replay_buffer is not None:
         replay_buffer.add(training_samples)
-        training_objects = replay_buffer.sample(n_trajectories=batch_size)
+        training_objects = replay_buffer.sample(n_trajectories=config.experiment.batch_size)
     else:
         training_objects = training_samples
 
@@ -68,10 +66,11 @@ for i in tqdm.trange(1000):
     unique_visited_states.update((t.numpy().tobytes(), 1) for t in trajectories.last_states.states_tensor)
     to_log = {"loss": loss.item(), "states_visited": states_visited, 'n_unique_states_visited': len(unique_visited_states)}
 
-    if i % validation_interval == 0:
-        validation_info, final_states_dist_pmf = validate(env, parametrization, n_validation_samples, visited_terminating_states, return_terminating_distribution=True)
+    if i % config.experiment.validation_interval == 0:
+        validation_info, final_states_dist_pmf = validate(env, parametrization, config.experiment.n_validation_samples, visited_terminating_states, return_terminating_distribution=True)
         to_log.update(validation_info)
         tqdm.tqdm.write(f"Iteration: {i}: {to_log}")
 
-        plt.matshow(final_states_dist_pmf.reshape(grid_height, grid_height).T)
-        plt.show()
+        if env.ndim == 2:
+            plt.matshow(final_states_dist_pmf.reshape(config.env.height, config.env.height))
+            plt.show()
