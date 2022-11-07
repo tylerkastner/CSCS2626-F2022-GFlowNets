2b6ede1375a42524e7c153dbf21585164f638ef8
Tyler Kastner <tylerkastner@Tylers-MacBook-Pro.local>
2022-11-07 10:01:41 -0500

    [tk] Create reward network

---
+++ networks/reward_network.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+
+class RewardNetwork(nn.Module):
+    def __init__(
+        self, 
+        state_dim,
+        hidden_dim1 = 128, 
+        out_features = 1, 
+    ):
+        super(RewardNetwork, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(state_dim, hidden_dim1),
+            nn.ReLU(),
+            nn.Linear(hidden_dim1, out_features),
+        )
+
+    def forward(self, x):
+        return self.net(x)  
